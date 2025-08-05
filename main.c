edb61053b5edcc6c0c15009637c92906091d5192
Sena Nur Bakır <150732354+ssenanb@users.noreply.github.com>
2025-08-05 15:23:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,50 +1,13 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
 #include "BME280_STM32.h"
-/* USER CODE END Includes */
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
 #define SIZE 5
 #define DELAY_MS 3000
-/* USER CODE END PD */
 
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc;
 
 I2C_HandleTypeDef hi2c1;
@@ -85,11 +48,7 @@ osSemaphoreId_t BinarySemHandle;
 const osSemaphoreAttr_t BinarySem_attributes = {
   .name = "BinarySem"
 };
-/* USER CODE BEGIN PV */
 
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
@@ -98,7 +57,6 @@ static void MX_ADC_Init(void);
 void StartSensorTask(void *argument);
 void StartUARTTask(void *argument);
 
-/* USER CODE BEGIN PFP */
 typedef struct{
 	float temperature;
 	float humidity;
@@ -111,20 +69,17 @@ typedef struct{
 } SensorValue;
 
 volatile uint8_t trigger_sensor = 0;
-/* USER CODE END PFP */
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
 // if the sound sensor is triggered, sensors start taking measurement and the LED is switched on
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
-if(GPIO_Pin == GPIO_PIN_3 && trigger_sensor == 0){
+	if(GPIO_Pin == GPIO_PIN_3 && trigger_sensor == 0){
 		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);
 		osSemaphoreRelease(BinarySemHandle);
 	}
 }
 
 void readGasSensor(int *gas){
-	static uint16_t readings[SIZE] = {0};
+    static uint16_t readings[SIZE] = {0};
     static uint8_t indx = 0;
     static uint16_t sum = 0;
 
@@ -133,127 +88,50 @@ void readGasSensor(int *gas){
     uint16_t gas_data = HAL_ADC_GetValue(&hadc);
     HAL_ADC_Stop(&hadc);
 
-	// the moving average filter is implemented
-   	sum -= readings[indx];
-   	readings[indx] = gas_data;
-   	sum += gas_data;
+    // the moving average filter is implemented
+    sum -= readings[indx];
+    readings[indx] = gas_data;
+    sum += gas_data;
     indx = (indx + 1) % SIZE;
 
-   	uint16_t filtered_value = sum / SIZE;
+    uint16_t filtered_value = sum / SIZE;
 
-   	/* The gas quality is represented as a percentage
-   	 * If the air is polluted, the A0 output of the gas sensor gives a high value and,
-     * If the air is clean, the A0 output of the gas sensor gives a low value
-     * To reverse this logic and make 100% represent clean air, I subtract the calculated value from 100
-     */
-   	*gas =  100 - (100 * (int)filtered_value / 4095);
+   /* The gas quality is represented as a percentage
+    * If the air is polluted, the A0 output of the gas sensor gives a high value and,
+    * If the air is clean, the A0 output of the gas sensor gives a low value
+    * To reverse this logic and make 100% represent clean air, I subtract the calculated value from 100
+    */
+     *gas =  100 - (100 * (int)filtered_value / 4095);
 }
-/* USER CODE END 0 */
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
   MX_USART1_UART_Init();
   MX_ADC_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
-
-  /* Init scheduler */
   osKernelInitialize();
-  /* Create the mutex(es) */
-  /* creation of i2cMutex */
-  i2cMutexHandle = osMutexNew(&i2cMutex_attributes);
 
-  /* creation of UARTMutex */
+  i2cMutexHandle = osMutexNew(&i2cMutex_attributes);
   UARTMutexHandle = osMutexNew(&UARTMutex_attributes);
 
-  /* USER CODE BEGIN RTOS_MUTEX */
-  /* add mutexes, ... */
-  /* USER CODE END RTOS_MUTEX */
-
-  /* Create the semaphores(s) */
-  /* creation of BinarySem */
   BinarySemHandle = osSemaphoreNew(1, 0, &BinarySem_attributes);
 
-  /* USER CODE BEGIN RTOS_SEMAPHORES */
-  /* add semaphores, ... */
-  /* USER CODE END RTOS_SEMAPHORES */
-
-  /* USER CODE BEGIN RTOS_TIMERS */
-  /* start timers, add new ones, ... */
-  /* USER CODE END RTOS_TIMERS */
-
-  /* Create the queue(s) */
-  /* creation of messageQueue */
   messageQueueHandle = osMessageQueueNew (16, sizeof(SensorValue), &messageQueue_attributes);
 
-  /* USER CODE BEGIN RTOS_QUEUES */
-  /* add queues, ... */
-  /* USER CODE END RTOS_QUEUES */
-
-  /* Create the thread(s) */
-  /* creation of SensorTask */
   SensorTaskHandle = osThreadNew(StartSensorTask, NULL, &SensorTask_attributes);
-
-  /* creation of UARTTask */
   UARTTaskHandle = osThreadNew(StartUARTTask, NULL, &UARTTask_attributes);
-
-  /* USER CODE BEGIN RTOS_THREADS */
-  /* add threads, ... */
-  /* USER CODE END RTOS_THREADS */
-
-  /* USER CODE BEGIN RTOS_EVENTS */
-  /* add events, ... */
-  /* USER CODE END RTOS_EVENTS */
-
-  /* Start scheduler */
+	
   osKernelStart();
-
-  /* We should never get here as control is now taken by the scheduler */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	
   while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+  {}
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -491,12 +369,9 @@ static void MX_GPIO_Init(void)
 /* USER CODE END Header_StartSensorTask */
 void StartSensorTask(void *argument)
 {
-  /* USER CODE BEGIN 5 */
-	uint32_t time = osKernelGetTickCount();
+    uint32_t time = osKernelGetTickCount();
     BME280_Config(OSRS_2, OSRS_16, OSRS_1, MODE_NORMAL, T_SB_0p5, IIR_16);
-	SensorValue value;
-
-  /* Infinite loop */
+    SensorValue value;
   for(;;)
   {
 	  if(trigger_sensor == 0){
@@ -507,10 +382,9 @@ void StartSensorTask(void *argument)
 
 	  if(trigger_sensor){
 	 	BME280_Measure(&value.bme280_data.temperature,
-					   &value.bme280_data.pressure,
- 					   &value.bme280_data.humidity);
+			       &value.bme280_data.pressure,
+ 			       &value.bme280_data.humidity);
 	 	readGasSensor(&value.gas);
-
 
 	 	osMessageQueuePut(messageQueueHandle, &value, 0, 0);
 	  }
@@ -518,23 +392,12 @@ void StartSensorTask(void *argument)
 	  time += DELAY_MS;
 	  osDelayUntil(time);
   }
-
-  /* USER CODE END 5 */
 }
 
-/* USER CODE BEGIN Header_StartUARTTask */
-/**
-* @brief Function implementing the UARTTask thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_StartUARTTask */
 void StartUARTTask(void *argument)
 {
-  /* USER CODE BEGIN StartUARTTask */
 	SensorValue received_value;
 	char buffer[100];
-  /* Infinite loop */
   for(;;)
   {
 	  if(osMessageQueueGet(messageQueueHandle, &received_value, NULL, osWaitForever) == osOK){
@@ -553,17 +416,8 @@ void StartUARTTask(void *argument)
 	  }
 	  osDelay(1000);
   }
-  /* USER CODE END StartUARTTask */
 }
 
-/**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM1 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
   /* USER CODE BEGIN Callback 0 */
@@ -608,3 +462,4 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+
